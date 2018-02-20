80c9446cb32835764af08878567b859e55ffd22f
Nora Gully <nora.gully@gmail.com>
2018-02-20 01:23:02 -0700

    Delete google api example, clean up CORS error in app.js

--- src/router.js
+++ src/router.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var controller = require('./controller');
 
-// router.get('/places', controller.getPlaces);
-router.post('/whytho/:keys', controller.whyTho);
 router.get('/gettweets', controller.getTweets);
 
 module.exports = router;
