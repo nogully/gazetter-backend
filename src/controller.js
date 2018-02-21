15914bc34e10f2573a00310f58bc19b7cc386336
Nora Gully <nora.gully@gmail.com>
2018-02-20 21:32:08 -0700

    Start functions for filtering tweets

--- src/controller.js
+++ src/controller.js
@@ -1,28 +1,18 @@
 const request = require('request')
 const Twitter = require('twitter')
-require('locus')
 const consumer_key = require('./consumerKey.js');
 const consumer_secret = require('./consumerSecret.js');
 const access_token_key = require('./accessTokenKey.js');
 const access_token_secret = require('./accessTokenSecret.js');
-// const API_KEY = // API KEY GOES HERE
-// const BASE_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=${API_KEY}`
+require('locus')
 
-function getPlaces(req, res, next) {
-  request(BASE_URL, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.send(body)
-    }
-  })
-}
+var client = new Twitter({ consumer_key: consumer_key, 
+                           consumer_secret: consumer_secret,
+                           access_token_key: access_token_key,
+                           access_token_secret: access_token_secret });
 
-function getTweets(request, resp, next){
-  console.log('getTweets run')
-  var client = new Twitter({ consumer_key: consumer_key, 
-                             consumer_secret: consumer_secret,
-                             access_token_key: access_token_key,
-                             access_token_secret: access_token_secret });
-   
+
+function getTweets(request, resp, next){  
   var params = {screen_name: 'noragully'};
 
   client.get('statuses/home_timeline', params, function(error, tweets, response) {
@@ -33,7 +23,11 @@ function getTweets(request, resp, next){
     }
   });
 }
- 
+
+function filterTweets(req, resp, next) { 
+  var params = {screen_name: 'noragully'};
+
+}
 
 
 module.exports = { 
