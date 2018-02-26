bc707c2be27d211c4a1377d7f9314f4f4c993b06
Nora Gully <nora.gully@gmail.com>
2018-02-26 08:43:01 -0700

    Delete filterTweets function for eval but try later

--- src/router.js
+++ src/router.js
@@ -3,6 +3,5 @@ var router = express.Router();
 var controller = require('./controller');
 
 router.get('/gettweets', controller.getTweets);
-// router.post('/filterTweets/:filter', controller.filterTweets)
 
 module.exports = router;
